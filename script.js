5d009993ba1f3fea65af2bcc48007197bc2fe643
Naveen Doddi <109824438+NaveenDoddi@users.noreply.github.com>
2024-01-19 20:20:17 +0530

    Updated

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function run(){
 
-    document.getElementById("table").innerHTML = ""
+    document.getElementById("div2").innerHTML = ""
 
     var input = document.getElementById("input").value
-    document.getElementById("para").style.visibility="visible"
+    
     var xhr = new XMLHttpRequest()
     var api = "https://app.zipcodebase.com/api/v1/search?apikey=fb6dbf50-6aef-11ed-8277-1b1a362cf12b&codes="+input
     xhr.open('GET',api)
     xhr.send()
     xhr.onload = function(){
-        var parsed =JSON.parse(xhr.response)
+        var parsed = JSON.parse(xhr.response)
         // console.log(parsed.results[input].length)
 
         for(let i=0; i<parsed.results[input].length; i++){
@@ -18,40 +18,35 @@ function run(){
                 var table = document.createElement("table")
 
                 var tr = document.createElement("tr")
-                var td = document.createElement("td")
-                td.innerText="Place:"+parsed.results[input][i].city
-                td.style.backgroundColor="blanchedalmond"
-                tr.append(td)
+                var th = document.createElement("th")
+                th.innerText="Place: "+parsed.results[input][i].city
+                th.className = "bg-dark text-light"
+                tr.append(th)
                 table.append(tr)
-                document.getElementById("table").append(table)
                 
                 var tr = document.createElement("tr")
                 var td = document.createElement("td")
-                td.innerText="Pincode:"+parsed.results[input][i].postal_code
+                td.innerText="Pincode: "+parsed.results[input][i].postal_code
                 tr.append(td)
                 table.append(tr)
-                document.getElementById("table").append(table)
     
                 var tr = document.createElement("tr")
                 var td = document.createElement("td")
-                td.innerText="District:"+parsed.results[input][i].province
+                td.innerText="District: "+parsed.results[input][i].province
                 tr.append(td)
                 table.append(tr)
-                document.getElementById("table").append(table)
     
                 var tr = document.createElement("tr")
                 var td = document.createElement("td")
-                td.innerText="State:"+parsed.results[input][i].state
+                td.innerText="State: "+parsed.results[input][i].state
                 tr.append(td)
                 table.append(tr)
-                document.getElementById("table").append(table)
                 
                 var tr = document.createElement("tr")
                 var td = document.createElement("td")
-                td.innerText="Country:"+parsed.results[input][i].country_code
+                td.innerText="Country: "+parsed.results[input][i].country_code
                 tr.append(td)
                 table.append(tr)
-                document.getElementById("table").append(table)
     
                 var tr = document.createElement("tr")
                 var td = document.createElement("td")
@@ -60,17 +55,25 @@ function run(){
                 var a = document.createElement("a")
                 a.href = "https://www.latlong.net/c/?lat="+latitude+"&long="+longitude
                 a.target = '_blank'
-                a.innerText = "Locate on Map"
+                a.innerText = "Locate on Map "
                 // a.style.color="black"
                 a.style.textDecoration="none"
-                td.append(a)
+
+                var icon = document.createElement("i")
+                icon.className = "fa-solid fa-map-location-dot fa-bounce"
+                
+                a.append(icon)
+                td.append(a,icon)
                 tr.append(td)
                 table.append(tr)
-                document.getElementById("table").append(table)
+                table.className = "col-xs-11 col-sm-5 col-md-3 col-lg-3"
+                document.getElementById("div2").append(table)
 
             }
 
            
         }
+        document.getElementById("para").style.visibility="visible"
     }
+    
 }
